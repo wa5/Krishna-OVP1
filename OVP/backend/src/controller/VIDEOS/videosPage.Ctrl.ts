0c55c72ef6106d479d9c9f93ea3d2f5c76ae11cb
wa5 <waseemslyar@gmail.com>
2022-07-29 11:21:00 +0530

    ggg

--- OVP/backend/src/controller/VIDEOS/videosPage.Ctrl.ts
+++ OVP/backend/src/controller/VIDEOS/videosPage.Ctrl.ts
@@ -1,7 +1,9 @@
 const { VideosUpload}= require('../../models')
 export const videosPage_Get=(req:any,res:any)=>{
     const users=VideosUpload.find({userid:res.locals.user._id}, (err:any,data:any)=>{
-        if(err)throw err;
+        if(err){
+          console.log("---",err)
+        }
         console.log("---",data)
         res.render('videosPage',{users:data})
           })
--- OVP/backend/src/controller/userdetails/userdetails.Ctrl.ts
+++ OVP/backend/src/controller/userdetails/userdetails.Ctrl.ts
@@ -1,4 +1,10 @@
-export const userdetails_Get=(req:any,res:any)=>{
+const { VideosUpload}= require('../../models')
+export const userdetails_Get=async(req:any,res:any)=>{
+    console.log("====1",res.locals.user._id)
+    const users= await VideosUpload.find({userid:res.locals.user._id})
+        
+        console.log("---",users)
+       
     res.render('userdetails')
 }
 export const userdetails_Post=(req:any,res:any)=>{
